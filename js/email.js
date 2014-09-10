854ff3c6046d9e741e3d224211b2d15d62c9ceec
mdelano <mike@mediasilo.com>
2014-09-10 18:54:55 -0400

    Email

---
+++ js/email.js
@@ -0,0 +1,66 @@
+// Create a function to log the response from the Mandrill API
+function log(obj) {
+    $('#response').text(JSON.stringify(obj));
+}
+
+// create a new instance of the Mandrill class with your API key
+var m = new mandrill.Mandrill('EFP0TtWypDVL5ieuKLyQ2w');
+// create a variable for the API call parameters
+var params = {
+    "key": "EFP0TtWypDVL5ieuKLyQ2w",
+    "message": {
+        "text": "",
+        "subject": "Website Inquiry",
+        "from_email": "",
+        "from_name": "",
+        "to": [
+            {
+                "email": "info@stackrise.com",
+                "name": "Mike",
+                "type": "to"
+            },
+            {
+                "email": "mike@stackrise.com",
+                "name": "Mike",
+                "type": "to"
+            }
+        ],
+        "headers": {
+            "Reply-To": "info@stackrise.com"
+        },
+        "important": false,
+        "track_opens": null,
+        "track_clicks": null,
+        "auto_text": null,
+        "auto_html": null,
+        "inline_css": null,
+        "url_strip_qs": null,
+        "preserve_recipients": null,
+        "view_content_link": null,
+        "tracking_domain": null,
+        "signing_domain": null,
+        "return_path_domain": null,
+        "merge": true,
+        "tags": [
+            "website-inquiry"
+        ],
+        "metadata": {
+            "website": "stackrise.com"
+        },
+        "images": [
+            {
+                "type": "image/png",
+                "name": "IMAGECID",
+                "content": "ZXhhbXBsZSBmaWxl"
+            }
+        ]
+    }
+}
+
+function sendTheMail(from_email, from_name, text) {
+    console.log("From Email", from_email, "From Name", from_name, "Text", text);
+    params.message.from_email = from_email;
+    params.message.from_name = from_name;
+    params.message.text = text + " " + from_email;
+    m.messages.send(params);
+}
